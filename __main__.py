04d2ec0446f6727d368a319eb5d0ad8ed5f92890
Wudext <jalchinn@yandex.ru>
2024-02-15 18:24:52 +0300

    f

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
-from data_analisys.functions import preprocessing, get_events
+from data_analisys import functions as f
 from settings import Settings
-
 
 if __name__ == "__main__":
     columns = ["Date/time", "P >5 MeV", "P >10 MeV"]
 
     settings = Settings().get_settings()
-    table = preprocessing(file=settings["path"],
-                          sep=settings["sep"],
-                          columns=columns)
-    events = get_events(table)
+    table = f.preprocessing(file=settings["path"],
+                            sep=settings["sep"],
+                            columns=columns)
+    # print(table)
+    f.create_graph(x_axis=table[table.keys()[0]].tolist(), y_axis=table[table.keys()[2]].tolist())
+    events = f.get_events(table=table, column="P >5 MeV", value=2.0)
     print(events)
-    print(len(events))
--- data_analisys/functions.py
+++ data_analisys/functions.py
@@ -1,31 +1,39 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import re
+from datetime import datetime
+from itertools import groupby
 
 
 def preprocessing(file, sep, columns):
     data = pd.read_csv(file, sep=sep, header=None)
     data.columns = columns
-    data.dropna(thresh=1)
 
+    col = data[data.keys()[0]].tolist()
+    for i in range(len(col)):
+        data[data.keys()[0]] = data[data.keys()[0]] \
+            .replace(col[i], datetime.strptime(col[i][:-7], "%Y-%m-%d %H:%M:%S"))
+
+    for j in range(1, 3):
+        col = data[data.keys()[j]].tolist()
+        for i in range(len(col)):
+            check = re.match(r" *[\d.]+ *", col[i])
+            if check:
+                data[data.keys()[j]] = data[data.keys()[j]].replace(col[i], float(col[i]))
+            else:
+                data[data.keys()[j]] = data[data.keys()[j]].replace(col[i], None)
+
+    data = data.dropna()
     return data
 
 
-def get_events(table: pd.DataFrame):
-    events = []
-    tmp = []
-    is_event = False
-
-    for i in range(1, table.shape[0]):
-        try:
-            val = float(table.iloc[i][table.keys()[2]])
-        except ValueError:
-            continue
-        if val > 0.4:
-            is_event = True
-            tmp.append(val)
-        else:
-            tmp.append(0.0)
-            if is_event:
-                events.append(tmp)
-            is_event = False
+def get_events(table: pd.DataFrame, column: str, value: float = 1.5):
+    events = [list(g) for k, g in groupby(table[column].tolist(), key=lambda x: x > value) if k]
 
     return events
+
+
+def create_graph(x_axis: list, y_axis: list):
+    plt.plot(x_axis, y_axis)
+    plt.semilogy()
+    plt.show()
